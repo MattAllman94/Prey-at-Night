279ddfed0eadd4ba01a943cd5f8f640548b0e1bf
MattAllman94 <62230366+MattAllman94@users.noreply.github.com>
2021-05-15 19:39:40 +1000

    Enemy AI
    Need to be fixed for Chase and Detect player

--- Assets/Scripts/NPC.cs
+++ Assets/Scripts/NPC.cs
@@ -11,18 +11,27 @@ public class NPC : Prey
     }
     public enum State
     {
-        Idle, Patrol, Detect, Hunt
+        Idle, Detect, Hunt
     }
 
     public EnemyType myType;
+    public State myState;
 
     NavMeshAgent agent;
     int currentWaypoint;
 
+    public GameObject player;
+    private float timer;
+    public float wanderRadius;
+    public float wanderTimer;
+    public float detectDistance;
+    public float undetectTimer;
+    public float chaseDistance;
 
     private void Start()
     {
         agent = GetComponent<NavMeshAgent>();
+        timer = wanderTimer;
 
         switch (myType)
         {
@@ -41,7 +50,7 @@ public class NPC : Prey
 
     void Update()
     {
-        switch(myType)
+        switch (myType)
         {
             case EnemyType.Civilian:
                 CivilianMovement();
@@ -54,6 +63,8 @@ public class NPC : Prey
                 break;
 
         }
+        timer += Time.deltaTime;
+
     }
 
     #region Movement
@@ -82,8 +93,86 @@ public class NPC : Prey
 
     public void MonsterMovement()
     {
+        float distToPlayer = Vector3.Distance(transform.position, player.transform.position);
 
+        switch (myState)
+        {
+            case State.Idle:
+                if (timer >= wanderTimer)
+                {
+                    Vector3 newPos = RandomNavSphere(transform.position, wanderRadius, -1);
+                    agent.SetDestination(newPos);
+                    timer = 0;
+                }
+                break;
+            case State.Detect:
+                agent.SetDestination(transform.position);
+                transform.LookAt(player.transform.position);
+                undetectTimer -= Time.deltaTime;
+                if (undetectTimer <= 0)
+                {
+                    if (distToPlayer <= detectDistance)
+                    {
+                        myState = State.Hunt;
+                        undetectTimer = 5;
+                    }
+                    else
+                    {
+                        myState = State.Idle;
+                    }
+                }
+                break;
+            case State.Hunt:
+                agent.SetDestination(player.transform.position);
+                if (distToPlayer > chaseDistance)
+                {
+                    myState = State.Detect;
+                }
+                break;
+        }
+
+        if (distToPlayer <= detectDistance)
+        {
+            if (Physics.Linecast(transform.position, player.transform.position, out RaycastHit hit))
+            {
+                if (hit.collider.CompareTag("Player"))
+                {
+                    if (myState != State.Hunt)
+                    {
+                        myState = State.Detect;
+                    }
+                }
+            }
+        }
+        else
+        {
+            myState = State.Idle;
+        }
+
+        if (distToPlayer >= chaseDistance)
+        {
+            if (Physics.Linecast(transform.position, player.transform.position, out RaycastHit hit))
+            {
+                if (hit.collider.CompareTag("Player"))
+                {
+                    myState = State.Hunt;
+                }
+            }
+        }
+        else
+        {
+            myState = State.Idle;
+        }
+    }
+    public static Vector3 RandomNavSphere(Vector3 origin, float dist, int layermask)
+    {
+        Vector3 randDirection = Random.insideUnitSphere * dist;
+        randDirection += origin;
+        NavMeshHit navHit;
+        NavMesh.SamplePosition(randDirection, out navHit, dist, layermask);
+        return navHit.position;
     }
 
     #endregion
+        
 }
